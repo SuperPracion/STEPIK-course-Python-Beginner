57dc2e178036c964b6b7668277004e3b49675f18
SuperPracion <pracion@bk.ru>
2022-08-24 16:50:08 +0300

    Завершение блока 11.1

---
+++ Section 11.1/1.py
@@ -0,0 +1,4 @@
+length_input = int(input())
+mass = [num + 1 for num in range(length_input)]
+
+print(mass)
---
+++ Section 11.1/2.py
@@ -0,0 +1,4 @@
+start_position = int(input())
+alphas_mass = [chr(symbol + 97) for symbol in range(start_position)]
+
+print(alphas_mass)
